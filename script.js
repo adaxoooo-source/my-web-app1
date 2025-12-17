83d43cb666cdc7321bd58cb06536f80598cced5f
adaxoooo-source <adaxoooo@icloud.com>
2025-12-17 16:47:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log("Bienvenue  My Web App!");
-console.log("My Web App est servi depuis Docker Desktop !");
+function goTo(sectionId) {
+  document.querySelectorAll('section').forEach(s => s.classList.remove('active'));
+  document.getElementById(sectionId).classList.add('active');
+
+  document.querySelectorAll('nav a').forEach(a => a.classList.remove('active'));
+  document.querySelectorAll('nav a').forEach(a => {
+    if (a.textContent.toLowerCase().includes(sectionId)) {
+      a.classList.add('active');
+    }
+  });
+}
+
